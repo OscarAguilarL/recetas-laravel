c9ee11bd9c23fb5a73a9746d61b28a6e84d5081f
Oscar Aguilar <aguilop.oscar@gmail.com>
2021-12-23 11:23:27 -0600

    Guardando recetas con modelo

--- app/Receta.php
+++ app/Receta.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Receta extends Model
 {
-    //
+
+    protected $fillable = [
+        'titulo', 'preparacion', 'ingredientes', 'imagen', 'categoria_id'
+    ];
+
+    // obtiene la categoria de la receta via fk
+    public function categoria()
+    {
+        return $this->belongsTo(CategoriaReceta::class);
+    }
 }
