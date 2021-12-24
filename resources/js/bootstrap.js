e7d2691d0afe6e6c1e516c44e7ddedc904ea255c
Oscar Aguilar <aguilop.oscar@gmail.com>
2021-12-23 18:55:05 -0600

    Relacion para mostrar el autor

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -39,3 +39,6 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 //     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
 //     encrypted: true
 // });
+
+// Instalar momentJs
+window.moment = require('moment');
